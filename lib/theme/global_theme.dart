91315fe4531cb8a75436157952cdf918425f4680
dev2820 <second28200@gmail.com>
2022-04-06 13:29:09 +0900

    feat/ global_theme 생성 및 main에서 lightTheme를 사용하도록 설정 (#8)
    앱의 전체적인 테마가 될 global_theme 클래스를 생성했고, 이 클래스를
    전체적으로 사용할 수 있게함.
    
    앞으로 GlobaTheme.~~ 식으로 접근해 특정 컬러를 사용하면된다.
    Colors.~~ 로 접근하는 일은 없도록 하자

---
+++ lib/theme/global_theme.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class GlobalTheme {
+  GlobalTheme();
+
+  static const Map<int, Color> _lightPrimaryPalette = <int, Color>{
+    900: Color(0xff285489),
+    800: Color(0xff3174ac),
+    700: Color(0xff3684c0),
+    600: Color(0xff3c97d4),
+    500: Color(0xff41a5e3),
+    400: Color(0xff4db2e7),
+    300: Color(0xff62bfeb),
+    200: Color(0xff89d1f2),
+    100: Color(0xffb6e3f7),
+    50: Color(0xffe2f4fc),
+  };
+  static const Map<int, Color> _blueGray = <int, Color>{
+    900: Color(0xff263238),
+    800: Color(0xff37474F),
+    700: Color(0xff455A64),
+    600: Color(0xff546E7A),
+    500: Color(0xff607D8B),
+    400: Color(0xff78909C),
+    300: Color(0xff90A4AE),
+    200: Color(0xffB0BEC5),
+    100: Color(0xffCFD8DC),
+    50: Color(0xffECEFF1),
+  };
+  static const Map<int, Color> _error = <int, Color>{
+    900: Color(0xffb00020),
+    800: Color(0xffbf152c),
+    700: Color(0xffcc1d33),
+    600: Color(0xffde2839),
+    500: Color(0xffed323b),
+    400: Color(0xffe84853),
+    300: Color(0xffde6c74),
+    200: Color(0xffea959b),
+    100: Color(0xfffbcad2),
+    50: Color(0xfffdeaee),
+  };
+  static const Color _iconColor = Color(0xff2B3137);
+
+  static const Color _lightPrimaryColor = Color(0xff0E7EB8);
+  static const Color _lightPrimaryVariantColor = Color(0xff004B72);
+  static const Color _lightBackgroundColor = Color(0xffFFFFFF);
+  static const Color _lightSurfaceColor = Color(0xffFFFFFF);
+  static const Color _lightErrorColor = Color(0xffDD3730);
+  static const Color _lightOnPrimaryColor = Color(0xff121212);
+  static const Color _lightOnSurfaceColor = Color(0xff121212);
+  static const Color _lightOnBackgroundColor = Color(0xff121212);
+  static const Color _lightOnErrorColor = Color(0xffFEFEFE);
+
+  static final ThemeData lightTheme = ThemeData(
+      appBarTheme: const AppBarTheme(
+    centerTitle: true, // 텍스트 중앙 배치
+    backgroundColor: Colors.transparent, // 배경 투명하게
+    elevation: 0, // 기본 degree는 0으로
+    titleTextStyle: TextStyle(
+        color: _lightOnBackgroundColor,
+        fontFamily: "Roboto",
+        fontWeight: FontWeight.bold,
+        fontSize: 20),
+  ));
+}
