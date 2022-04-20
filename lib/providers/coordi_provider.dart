5d37a9fadd184720e279c00e78fea6f6f8722208
Tuna-is-DW <ydw5210@gmail.com>
2022-04-20 20:23:43 +0900

    feature/svg 이미지 파일 경로 메서드 생성(#92)
    -이미지 파일경로 생성하는 메서드 만듦

--- lib/providers/coordi_provider.dart
+++ lib/providers/coordi_provider.dart
@@ -56,7 +56,6 @@ class CoordiProvider with ChangeNotifier {
     Uri uri = Uri.parse(
         "https://us-central1-sweather-46fbf.cloudfunctions.net/api/coordi/recommand?gender=$userGender&stemp=${forecastList[forecastIdx].getSTemp}&isRain=${forecastList[forecastIdx].getSky == '비' ? true : false}&isSnow=${forecastList[forecastIdx].getSky == '눈' ? true : false}&windSpeed=${forecastList[forecastIdx].getWindSpeed}");
     var response = await http.get(uri);
-    // print(response.body);
     return response.body;
   }
 
