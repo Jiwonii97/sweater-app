658309bb32a310a9128cd1ba34cdccaf2bc3a294
dev2820 <second28200@gmail.com>
2022-04-24 18:36:57 +0900

    feat/ UserProvider를 새로 생긴 User로 갱신 (#86)
    User 클래스가 새로 생김에 따라 UserProvider의 내용도 새롭게 갱신했다.

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sweater/module/Constitution.dart';
+import 'package:sweater/module/gender.dart';
+import 'package:sweater/module/user.dart';
+
+// User 클래스 정의
+class UserProvider extends ChangeNotifier {
+  User _user = User(Gender.man, Constitution.feelNormal);
+  int get gender {
+    return _user.gender;
+  }
+
+  int get constitution {
+    return _user.constitution;
+  }
+
+  UserProvider() {
+    initUserInfo();
+  }
+
+  void initUserInfo() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      _user.gender = (prefs.getInt('gender') ?? Gender.man);
+      _user.constitution =
+          prefs.getInt('constitution') ?? Constitution.feelNormal;
+    } catch (e) {
+      print('UserProvider의 initUserInfo에서 에러가 발생했습니다. $e');
+    }
+
+    notifyListeners();
+  }
+
+  void changeGender(int newGender) async {
+    try {
+      _user.gender = newGender;
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setInt('gender', newGender);
+    } catch (e) {
+      print('UserProvider의 changeGender에서 에러가 발생했습니다. $e');
+    }
+    notifyListeners();
+  }
+
+  void changeConstitution(int newConstitution) async {
+    try {
+      _user.constitution = newConstitution;
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setInt('constitution', newConstitution);
+    } catch (e) {
+      print('UserProvider의 changeConstitution에서 에러가 발생했습니다. $e');
+    }
+    notifyListeners();
+  }
+}
