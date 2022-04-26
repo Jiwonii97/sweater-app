3faefae8a9b1eebd6d7757c2a8b667e686e05338
dev2820 <second28200@gmail.com>
2022-04-27 00:30:24 +0900

    feat/ coordi 클래스를 cloth 클래스에서 분리해냄 (#108)
    기존 cloth 클래스에 같이 있던 coordi 클래스를 분리해냄(관심사분리)

--- lib/module/cloth.dart
+++ lib/module/cloth.dart
@@ -3,45 +3,6 @@ import 'dart:io' as io;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
-class Coordi {
-  final String _url;
-  final List<Cloth> _clothes;
-  final List<int> _temperature;
-  final int _gender;
-
-  Coordi(this._url, this._clothes, this._temperature, this._gender);
-
-  String get url => _url;
-  List<Cloth> get clothes => _clothes;
-  List<int> get temperature => _temperature;
-  int get gender => _gender;
-
-  List<String> getCoordiInfo() {
-    List<String> result = [];
-    for (int i = 0; i < clothes.length; i++) {
-      result.add(clothes[i].getClothInfo());
-    }
-    return result;
-  }
-
-  List<String> getIllustUrl() {
-    List<String> result = []; //[아우터, 상의, 하의] 순으로 일러스트 주소 저장
-
-    if (clothes.length == 3) {
-      //아우터 있는 코디
-      result.add(clothes[0].getSVGFilePath()); //아우터
-      result.add(clothes[1].getSVGFilePath()); //상의
-      result.add(clothes[2].getSVGFilePath()); //하의
-    } else {
-      //아우터 없는 코디
-      result.add("");
-      result.add(clothes[0].getSVGFilePath()); //상의
-      result.add(clothes[1].getSVGFilePath()); //하의
-    }
-    return result;
-  }
-}
-
 class Cloth {
   final String _majorCategory;
   final String _minorCategory;
