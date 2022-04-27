e85bc316c5ab0e1dbf5c60e36d365b8b0f25334d
dev2820 <second28200@gmail.com>
2022-04-27 18:26:16 +0900

    feat/ Coordi 클래스 테스트 코드 작성 (#108)
    Coordi 클래스에 대해 실질적으로 동작하는 테스트 코드 작성

--- test/units/coordi_test.dart
+++ test/units/coordi_test.dart
@@ -1,3 +1,4 @@
+import 'package:sweater/module/cloth.dart';
 import 'package:sweater/module/coordi.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -9,16 +10,32 @@ import 'package:flutter_test/flutter_test.dart';
 
 main() {
   test('코디 생성', () {
-    //Coordi instance = Coordi(this._url, this._clothes);
+    Cloth top = Cloth("top", "sleeve", "FFFFFF", "흰 크롭 반팔 티셔츠");
+    Cloth bottom = Cloth("bottom", "jeans", "9AB3D2", "연청 반바지");
+    Coordi instance =
+        Coordi("https://www.musinsa.com/mz/brandsnap?_m=&p=", [top, bottom]);
   });
 
   test('Coordi getCoordiInfo 테스트', () {
-    //Coordi 에 '흰 크롭 반팔 티셔츠'를 반환하는 Cloth 클래스, '연청 짧은 청바지'를 반환하는 Cloth 클래스를 넣어 생성할 때
-    //getCoordiInfo의 실행 결과는 ['흰 크롭 반팔 티셔츠','연청 짧은 청바지']이다.
+    //Coordi 에 '흰 크롭 반팔 티셔츠'를 반환하는 Cloth 클래스, '연청 청바지'를 반환하는 Cloth 클래스를 넣어 생성할 때
+    //getCoordiInfo의 실행 결과는 ['흰 크롭 반팔 티셔츠','연청 청바지']이다.
+    Cloth top = Cloth("top", "sleeve", "FFFFFF", "흰 크롭 반팔 티셔츠");
+    Cloth bottom = Cloth("bottom", "jeans", "9AB3D2", "연청 반바지");
+    Coordi instance =
+        Coordi("https://www.musinsa.com/mz/brandsnap?_m=&p=", [top, bottom]);
+    expect(instance.getCoordiInfo(), ['흰 크롭 반팔 티셔츠', '연청 청바지']);
   });
 
   test('Coordi getIllustUrl 테스트', () {
     //Coordi 에 '흰 크롭 반팔 티셔츠'를 반환하는 Cloth 클래스, '연청 짧은 청바지'를 반환하는 Cloth 클래스를 넣어 생성할 때
     //getIllustUrl 실행 결과는 ['asset/cloth/top/crop_short_sleeve.svg','asset/cloth/bottom/short_jeans.svg']이다.
+    Cloth top = Cloth("top", "sleeve", "FFFFFF", "흰 크롭 반팔 티셔츠");
+    Cloth bottom = Cloth("bottom", "jeans", "9AB3D2", "연청 반바지");
+    Coordi instance =
+        Coordi("https://www.musinsa.com/mz/brandsnap?_m=&p=", [top, bottom]);
+    expect(instance.getCoordiInfo(), [
+      'asset/cloth/top/short_sleeve.svg',
+      'asset/cloth/bottom/short_jeans.svg'
+    ]);
   });
 }
