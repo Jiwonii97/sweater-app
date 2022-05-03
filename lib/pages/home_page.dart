73a2b5fdf9accafec48fad6d7cbb3e9370a8456d
Tuna-is-DW <ydw5210@gmail.com>
2022-05-03 21:09:05 +0900

    chore/앱 이름 변경 sweater->스웨더(#???)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,7 +29,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
-  final String _title = "SWEATER";
+  final String _title = "스웨더";
   late Timer weatherUpdateTimer;
   EventEmitter weatherUpdateEmitter = new EventEmitter();
   EventEmitter coordiUpdateEmitter = new EventEmitter();
