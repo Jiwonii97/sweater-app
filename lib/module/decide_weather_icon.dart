818272aedb1c88d08cc5923ffa32b0b02e9f8944
Tuna-is-DW <ydw5210@gmail.com>
2022-05-03 21:01:41 +0900

    chore/밤일때 달 이미지로 변경(#132)

--- lib/module/decide_weather_icon.dart
+++ lib/module/decide_weather_icon.dart
@@ -3,13 +3,24 @@ import 'package:sweater/providers/weather_provider.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 Widget decideWeatherIcon(HourForecast hourForecast, bool isNow) {
+  int currentHour = DateTime.now().hour;
+  bool isNight = true;
+  if (6 <= currentHour && currentHour <= 18) {
+    isNight = false;
+  }
   switch (hourForecast.getSky) {
     case "맑음":
-      return SvgPicture.asset("assets/weather/sunny.svg",
-          width: isNow ? 80.0 : 36.0, height: isNow ? 80.0 : 36.0);
+      return SvgPicture.asset(
+          isNight ? "assets/weather/night.svg" : "assets/weather/sunny.svg",
+          width: isNow ? 80.0 : 36.0,
+          height: isNow ? 80.0 : 36.0);
     case "구름많음":
-      return SvgPicture.asset("assets/weather/cloudy.svg",
-          width: isNow ? 80.0 : 36.0, height: isNow ? 80.0 : 36.0);
+      return SvgPicture.asset(
+          isNight
+              ? "assets/weather/cloudyNight.svg"
+              : "assets/weather/cloudy.svg",
+          width: isNow ? 80.0 : 36.0,
+          height: isNow ? 80.0 : 36.0);
     case "흐림":
       return SvgPicture.asset("assets/weather/mostlyCloudy.svg",
           width: isNow ? 80.0 : 36.0, height: isNow ? 80.0 : 36.0);
@@ -25,6 +36,7 @@ Widget decideWeatherIcon(HourForecast hourForecast, bool isNow) {
     case "소나기":
       return SvgPicture.asset("assets/weather/shower.svg",
           width: isNow ? 80.0 : 36.0, height: isNow ? 80.0 : 36.0);
+    default:
       break;
   }
   return Text("");
