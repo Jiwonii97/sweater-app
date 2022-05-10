e7ae976ef23f3ab4b01bb9438983c8794a66f2fa
Tuna-is-DW <ydw5210@gmail.com>
2022-05-10 23:11:54 +0900

    refactor/주석 제거(#170)

--- lib/widgets/guide_text.dart
+++ lib/widgets/guide_text.dart
@@ -1,9 +1,7 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:wrapped_korean_text/wrapped_korean_text.dart';
 
 class GuideText extends StatelessWidget {
-  // final String guide;
   final String guideText;
   GuideText({
     Key? key,
@@ -14,11 +12,7 @@ class GuideText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.fromLTRB(60, 90, 60, 0),
-      //   padding:EdgeInsets.all(80),
-      // child: Text(
       child: WrappedKoreanText(
-        // child: WordBreakText(
-        // guidePhrase[guide],
         guideText,
         style: Theme.of(context).textTheme.subtitle2?.copyWith(
             color:
