2999dd2d9f545c66606e6b56700bf3c2f194b3a6
spaff13 <spaff316@gmail.com>
2022-05-12 22:45:51 +0900

    feat/ coordi_section 리팩토링

--- lib/widgets/card_container.dart
+++ lib/widgets/card_container.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 
 class CardContainer extends StatelessWidget {
   final Widget child;
+  final double elevation;
   const CardContainer({
     Key? key,
     required this.child,
+    this.elevation = 0,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
         child: Card(
-            elevation: 0.0,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
--- lib/widgets/coordi_section.dart
+++ lib/widgets/coordi_section.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:sweater/providers/coordi_provider.dart';
-import 'package:sweater/widgets/change_coordi_button.dart';
-import 'package:sweater/providers/weather_provider.dart';
-import 'package:sweater/theme/global_theme.dart';
-import 'dart:ui';
-import 'package:sweater/theme/sweater_icons.dart';
 import 'package:sweater/widgets/loading.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter/cupertino.dart';
 // import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:sweater/widgets/card_container.dart';
 
@@ -21,13 +13,9 @@ class CoordiSection extends StatefulWidget {
 }
 
 class _CoordiSectionState extends State<CoordiSection> {
-  static int prevPageIndex = 999;
-
   @override
   Widget build(BuildContext context) {
     final controller = PageController(viewportFraction: 0.8, initialPage: 999);
-    var _coordiIndexConsumer = Provider.of<CoordiProvider>(context);
-    int coordiIdx = _coordiIndexConsumer.coordiIdx;
     return context.watch<CoordiProvider>().isUpdateCoordiState
         ? Container(
             padding: const EdgeInsets.symmetric(vertical: 16),
@@ -38,23 +26,15 @@ class _CoordiSectionState extends State<CoordiSection> {
                       fontWeight: FontWeight.bold,
                     ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 16,
               ),
               context.watch<CoordiProvider>().coordiList.isEmpty
                   ? const Text("no data")
-                  : Container(
+                  : SizedBox(
                       height: 324,
                       child: PageView.builder(
                           controller: controller,
-                          onPageChanged: (index) {
-                            if (prevPageIndex > index)
-                              context.read<CoordiProvider>().prevCoordi();
-                            else
-                              context.read<CoordiProvider>().nextCoordi();
-                            prevPageIndex = index;
-                          },
-                          // itemCount: pages.length,
                           itemBuilder: (_, index) {
                             return CardContainer(
                                 child: CoordiView(
@@ -82,19 +62,18 @@ class _CoordiSectionState extends State<CoordiSection> {
 }
 
 class CoordiView extends StatelessWidget {
-  List coordi;
-  List coordiIllust;
-  CoordiView({Key? key, required this.coordi, required this.coordiIllust})
+  final List coordi;
+  final List coordiIllust;
+  const CoordiView({Key? key, required this.coordi, required this.coordiIllust})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String outer, top, bottomt;
-    return Container(
+    return SizedBox(
       width: 288,
       height: 288,
       child: Container(
-        margin: EdgeInsets.symmetric(vertical: 16),
+        margin: const EdgeInsets.symmetric(vertical: 16),
         child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
