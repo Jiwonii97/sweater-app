a192a1f0a5af1dc40bb4a07e6e641c186d1d6041
dev2820 <second28200@gmail.com>
2022-05-17 23:47:49 +0900

    feat/ coordi 클래스에 style 추가(#180)
    style 정보를 띄우기 위한 style 정보 추가 및 테스트 작성

--- lib/module/coordi.dart
+++ lib/module/coordi.dart
@@ -3,11 +3,13 @@ import 'package:sweater/module/cloth.dart';
 class Coordi {
   final String _url;
   final List<Cloth> _clothes;
+  final String _style;
 
-  Coordi(this._url, this._clothes);
+  Coordi(this._url, this._clothes, this._style);
 
   String get url => _url;
   List<Cloth> get clothes => _clothes;
+  String get style => _style;
 
   List<String> getCoordiInfo() {
     List<String> result = [];
