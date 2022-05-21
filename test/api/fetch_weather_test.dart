522112ee4d4a4f3f50dbd8674c72eb50c75bfa69
dev2820 <second28200@gmail.com>
2022-05-21 15:57:29 +0900

    fix/ HANDSHAKE CERTIFICATE_VERIFY_FAILED 에러 해결
    get->post로 요청이 바뀌면서 certification에러가 나는 듯 하다. 우선
    테스트에서 HttpVerify를 true로 하도록 수정해줬다

--- test/api/fetch_weather_test.dart
+++ test/api/fetch_weather_test.dart
@@ -12,12 +12,14 @@ import 'package:sweater/module/coordi.dart';
 import 'package:sweater/module/forecast.dart';
 import 'package:sweater/module/gender.dart';
 import 'package:sweater/module/user.dart';
+import 'package:sweater/my_http_overrides.dart';
 import 'package:sweater/providers/weather_provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 main() async {
   dotenv.testLoad(fileInput: File('test/.env').readAsStringSync());
 
+  HttpOverrides.global = MyHttpOverrides();
   test('fetch_weather 통신 테스트', () async {
     int predictMax = 12;
     int nx = 61, ny = 124;
